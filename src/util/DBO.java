a5cf6a648afe47875c1e95b2034243146f57c1b1
Esdras de Morais <esdrasbsbmorais@gmail.com>
2023-11-15 09:40:17 -0300

    deixando o projeto em arquitetura MVC

---
+++ src/util/DBO.java
@@ -0,0 +1,21 @@
+package util;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+import models.Pessoa;
+
+public class DBO {
+    private Connection connection;
+
+   
+    public DBO(Connection connection) {
+      this.connection = connection;
+    }
+
+    public void conectar(Connection connection) throws SQLException {
+        this.connection = connection;
+    }
+}
